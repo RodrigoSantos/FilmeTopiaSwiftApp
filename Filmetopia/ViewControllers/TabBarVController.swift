eb44b1201374eff0a522cfd4445631053dde26b4
Rodrigo <rodrigo.oliveira@mercos.com>
2024-07-11 17:12:33 -0300

    Criando FavoriteCollectionResableViw
    Classe responssavel pela construção da Celula

---
+++ Filmetopia/ViewControllers/TabBarVController.swift
@@ -0,0 +1,12 @@
+//
+//  TabeBarVController.swift
+//  Filmetopia
+//
+//  Created by Rodrigo Oliveira on 11/07/24.
+//
+
+import UIKit
+
+class TabBarVController: UITabBarController {
+
+}
---
+++ Filmetopia/ViewControllers/TabBarVControllerViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TabBarVControllerViewController.swift
+//  Filmetopia
+//
+//  Created by Rodrigo Oliveira on 11/07/24.
+//
+
+import UIKit
+
+class TabBarVControllerViewController: UITabBarController {
+
+    //MARK - View life cyrcle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK - Class Methods
+   
+    private func setupTabBarViewController() {
+        let home = UINavigationController(rootViewController: MoviesViewController())
+        let symbolConfiguration = UIImage.SymbolConfiguration(scale: .medium)
+        
+        let homeSymbol = UIImage(systemName: "film", withConfiguration: symbolConfiguration)
+        home.tabBarItem.image = homeSymbol
+        home.tabBarItem.title = "Filmes Populares"
+        
+        
+        let favoritos = UINavigationController(rootViewController: FavoriteMoviesViewController())
+        let heartSymbol = UIImage(systemName: "heart", withConfiguration: symbolConfiguration)
+        favoritos.tabBarItem.image = heartSymbol
+        favoritos.tabBarItem.title = "Favoritos"
+        
+        self.setViewControllers([home, favoritos], animated: true)
+        
+        
+    }
+}
